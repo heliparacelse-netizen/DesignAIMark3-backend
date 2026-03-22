614d7cdd031e7784319c735d19b6021271425a75
heliparacelse-netizen <heliparacelse@gmail.com>
2026-03-22 19:30:28 +0000

    Fix HuggingFace API URL

--- src/routes/generate.ts
+++ src/routes/generate.ts
@@ -49,7 +49,7 @@ router.post('/', requireAuth, requireTokens, async (req: any, res: any) => {
       try {
         console.log(`[Generate] Calling HuggingFace API...`);
         const response = await fetch(
-          'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0',
+          'https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0',
           {
             method: 'POST',
             headers: {
